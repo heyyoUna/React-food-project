f2a81d592483067a01367732f55fadb018c637ea
Karin Hsin <karinhsin@gmail.com>
2021-10-28 01:44:02 +0800

    新增會員註冊功能

--- src/pages/Member/MemberSignup.js
+++ src/pages/Member/MemberSignup.js
@@ -2,27 +2,75 @@ import { withRouter } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
 function MemberSignup(props) {
-  console.log(props)
+  // console.log(props)
+  // const email = document.querySelector('#email');
 
-  const { auth, setAuth } = props
+  // const keyHandler = event => {
+  //   // console.log(event.type, event.key);
+  //   // TODO: 檢查格式是否符合 email 格式
 
+  //   const usp = new URLSearchParams();
+  //   usp.set('email', email.value.toLowerCase().trim());
+  //   //email沒給值就會跳錯
+
+  //   fetch('/account-check?' + usp).then(r => r.json()).then(data => {
+  //     if (data.used) {
+  //       // email 已被使用
+  //       email.style.border = '2px solid red';
+  //     } else {
+  //       email.style.border = '1px solid #CCCCCC';
+  //     }
+  //   });
+  // }
+  // email.addEventListener('input', keyHandler);
+
+   const handleSubmit = (e) => {
+    //這行一定要寫 為了阻擋form的預設送出行為
+    e.preventDefault()
+
+    // ex. 以下用fetch api/axios送到伺服器
+     // // TODO: 欄位檢查
+     const fd = new FormData(document.memberForm);
+     console.log(new URLSearchParams(fd).toString())
+     fetch('http://localhost:3002/signup', {
+       method: 'POST',
+       headers: {
+         'Content-Type': 'application/x-www-form-urlencoded'
+       },
+       body: new URLSearchParams(fd).toString(),
+     }).then(r => r.json()).then(obj => {
+       console.log(JSON.stringify(obj, null, 4));
+       if (obj.success) {
+         alert('註冊成功');
+         <Link to = '/login' ></Link>;
+       } else {
+         alert('註冊失敗\n' + (obj.error || ''));
+       }
+     });
+  }
+  
   return (
     <>
+    <form name="memberForm" onSubmit={handleSubmit}>
       <div className="karin-signup-container">
         <div className="karin-signup">
           <h1 id="karin-signup-h1">Sign up</h1>
           <div className="karin-form-group">
             <input
               type="email"
+              name="email"
               className="karin-form-control"
-              aria-describedby="emailHelp"
-              placeholder="Email"/>
+              placeholder="Email"
+              required/>
           </div>
           <div className="karin-form-group">
             <input
               type="password"
+              name="password"
               className="karin-form-control"
-              placeholder="Password"/>
+              placeholder="Password" 
+              minLength="5"  //最少要輸入5個字元
+              required/>
           </div>
           <div className="karin-form-group form-check">
             <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
@@ -43,6 +91,7 @@ function MemberSignup(props) {
       >
         {auth ? '登出' : '登入'}
       </button> */}
+      </form>
     </>
   )
 }
