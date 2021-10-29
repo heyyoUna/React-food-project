78f18759f4072ad2169b1e0fc1f1a8cde91c8f0f
Kelly <yijinwu5@gmail.com>
2021-10-29 14:45:41 +0800

    篩選評分

--- src/components/Restaurant/MapButtonGroup.js
+++ src/components/Restaurant/MapButtonGroup.js
@@ -4,30 +4,31 @@ import { RiMapPinLine } from 'react-icons/ri'
 import { imgUrl } from '../../config'
 
 function MapButtonGroup(props) {
+  const { linkFunction } = props
 
-  const {linkFunction } = props;
+  return (
+    <div className="row ">
+      <div className="col-md-3 col-6 ">
+        <button
+          type="button"
+          class="orange-btn"
+          onClick={linkFunction}
+        >
+          <RiMapPinLine
+            style={{
+              color: '#FB6107',
+              fontSize: '24px',
+              marginBottom: '4px',
+            }}
+          />{' '}
+          {props.name}
+        </button>
+      </div>
 
-  return (    
-    
-      <div className="row ">
-        <div className="col-md-3 col-6 ">
-          <button type="button" class="orange-btn" onClick={linkFunction}>
-            <RiMapPinLine
-              style={{
-                color: '#FB6107',
-                fontSize: '24px',
-                marginBottom: '4px',
-              }}
-            />{' '}
-            {props.name}
-          </button>
-        </div>
-        
-        {/* 從restaurant 三個sortbutton */}
-        {props.children}
+      {/* 從restaurant 三個sortbutton */}
+      {props.children}
 
-
-        {/* <div className="col-md-4  col-6 ">
+      {/* <div className="col-md-4  col-6 ">
           <select
           // className="form-select form-select-sm"
             // aria-label=".form-select-sm "
@@ -42,7 +43,7 @@ function MapButtonGroup(props) {
             <option value="2">最低評分</option>
           </select>
         </div> */}
-      </div>
+    </div>
   )
 }
 
