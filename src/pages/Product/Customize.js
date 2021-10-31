ee29363db7d13b7e0e32c1622554a880b85cb36d
Doris Liu <chiayuu.liu@gmail.com>
2021-10-31 17:52:55 +0800

    商城客製化基本計算功能

--- src/pages/Product/Customize.js
+++ src/pages/Product/Customize.js
@@ -1,9 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect  } from 'react'
+
+// 引用元件
 import ProductCard from './../../components/Product/ProductCard'
 import Target from '../../components/Product/Target'
+import Clientinfo from '../../components/Product/Clientinfo'
+
+
 function Customize(props) {
+  // 運動習慣狀態
   const [target, setTarget] = useState('')
   const [exercises, setExercises] = useState('')
+  // 基本資料狀態
+  const [gender, setGender] = useState('男')
+  const [years, setYears] = useState('')
+  const [height, setHeight] = useState('')
+  const [weight, setWeight] = useState('')
+  // 建議區
+  // 每日消耗熱量
+  const [TDEE, setTDEE] = useState(0)
+  // 建議攝取熱量
+  const [sugCal, setSugCal] = useState(0)
+  // 建議攝取蛋白質
+  const [sugProtein, setSugProtein] = useState(0)
+
+  useEffect(() => {
+    setSugCal(TDEE)
+    setSugProtein(Math.ceil(weight*1.2))
+  }, [TDEE])
+
+  //  每日建議熱量計算
+
   return (
     <>
       <div className="pd-client-banner d-flex">
@@ -14,51 +40,30 @@ function Customize(props) {
             setTarget={setTarget}
             exercises={exercises}
             setExercises={setExercises}
+            TDEE={TDEE}
           />
         </div>
         {/* 右區-使用者資料------- */}
         <div className="pd-client-wrap d-flex col-lg-5 col-md12">
           <div className="pd-client-info d-flex">
-            <div>
-              <label for="gender">性別</label>
-              <select
-                className="pd-gender"
-                name="gender"
-                id="gender"
-              >
-                <option value="">男</option>
-                <option value="">女</option>
-              </select>
-              <label for="years">年齡</label>
-              <input
-                type="number"
-                name="years"
-                value=""
-                className="pd-years"
-              />
-            </div>
-            <div>
-              <label for="height">身高</label>
-              <input
-                type="number"
-                name="height"
-                value=""
-                className="pd-height"
-              />
-              <label for="weight">體重</label>
-              <input
-                type="number"
-                name="weight"
-                value=""
-                className="pd-weight"
-              />
-            </div>
+            <Clientinfo
+              gender={gender}
+              setGender={setGender}
+              years={years}
+              setYears={setYears}
+              height={height}
+              setHeight={setHeight}
+              weight={weight}
+              setWeight={setWeight}
+              setTDEE={setTDEE}
+            />
           </div>
           <div className="pd-suggest d-flex">
-            <p className="dkgreen">每日消耗熱量1500大卡</p>
+          {/* {calculate()} */}
+            <p className="dkgreen">每日消耗熱量{TDEE}大卡</p>
             <p className="pd-day">建議每日攝取</p>
-            <p className="dkgreen">熱量1500大卡</p>
-            <p className="dkgreen">蛋白質50克</p>
+            <p className="dkgreen">熱量{sugCal}大卡</p>
+            <p className="dkgreen">蛋白質{sugProtein}克</p>
           </div>
           <button className="pd-client-btn">
             查看飲食推薦
