48919f994da9d56504f26a7f1bc204015ea7d1bb
Karin Hsin <karinhsin@gmail.com>
2021-11-08 19:29:41 +0800

    修改食譜輪盤跳出卡片功能

--- src/pages/Game/GameRecipe.js
+++ src/pages/Game/GameRecipe.js
@@ -3,11 +3,15 @@ import { Link, withRouter } from 'react-router-dom'
 import spinWheel from './spinWheel.png'
 import fork from './fork.png'
 import { Modal } from 'react-bootstrap';
+import { API_img } from '../../config/index'
+import ArCardTxtRecipe from '../../components/article/ArCardTxtRecipe'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
 
 function GameRecipe(props) {
   const [show, setShow] = useState(false)
   const [rotate, setRotate] = useState(false)
-  const [recipe, setRrecipe] = useState({})
+  const [recipe, setRecipe] = useState({})
 
   const handleRandom = () => {
     fetch(`http://localhost:3002/game/random-artrecipe-get`, {
@@ -15,14 +19,14 @@ function GameRecipe(props) {
     }).then(obj => obj.json())
       .then(obj => {
         if (obj.success) {
-          setRrecipe(obj.data)
+          setRecipe(obj.data)
           setRotate(true)
           setTimeout(() => {
             handleShow()
             setRotate(false)
           }, 1500);
         } else {
-          alert(obj.error)
+          Swal.fire(obj.error)
         }
       })
   }
@@ -48,6 +52,14 @@ function GameRecipe(props) {
           </button>
           <div className="karin-game-again" onClick={handleRandom}>再轉一次</div>
         </div>
+        <Modal show={show} centered={true} contentClassName={'game-card-wrap'} onHide={handleClose}>
+          <ArCardTxtRecipe
+            sid={recipe.sid}
+            ar_pic={recipe.ar_pic}
+            ar_title={recipe.ar_title}
+            isNotLiked={recipe.isLiked}
+          />
+        </Modal>
       </div>
     </>
   )
